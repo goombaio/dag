0eea1fa04c5427128e6b68593eb3c8ec803d7715
Raül Pérez <repejota@gmail.com>
2018-09-10 01:45:47 +0200

    Add predecessors support and some typos

--- dag.go
+++ dag.go
@@ -171,7 +171,7 @@ func (d *DAG) Successors(vertex *Vertex) ([]*Vertex, error) {
 
 	_, found := d.Vertices.Get(vertex.ID)
 	if found != true {
-		return successors, fmt.Errorf("vertex not found in the graph")
+		return successors, fmt.Errorf("vertex %s not found in the graph", vertex.ID)
 	}
 
 	for _, v := range vertex.Children.Values() {
@@ -179,6 +179,22 @@ func (d *DAG) Successors(vertex *Vertex) ([]*Vertex, error) {
 	}
 
 	return successors, nil
+}
+
+// Predecessors return vertices that are parent of a given vertex.
+func (d *DAG) Predecessors(vertex *Vertex) ([]*Vertex, error) {
+	var predecessors []*Vertex
+
+	_, found := d.Vertices.Get(vertex.ID)
+	if found != true {
+		return predecessors, fmt.Errorf("vertex %s not found in the graph", vertex.ID)
+	}
+
+	for _, v := range vertex.Parents.Values() {
+		predecessors = append(predecessors, v.(*Vertex))
+	}
+
+	return predecessors, nil
 }
 
 // String implements stringer interface and prints an string representation
